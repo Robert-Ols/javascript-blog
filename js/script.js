823e1e60f0e78b7768c1a9e98ebec374aec02efe
Robert Olszewski <robert.olszew40@gmail.com>
2023-01-28 11:24:13 +0100

    Add pop up article href attribute from the clicked link in js/script.js

--- js/script.js
+++ js/script.js
@@ -26,12 +26,14 @@ const titleClickHandler = function (event) {
     activeArticle.classList.remove("active");
   }
 
-  /* get 'href' attribute from the clicked link */
+  /* [DONE] get 'href' attribute from the clicked link */
+  const articleSelector = clickedElement.getAttribute ("href")  
  
- 
-  /* find the correct article using the selector (value of 'href' attribute) */
-  
-  /* add class 'active' to the correct article */
+  /* [DONE]find the correct article using the selector (value of 'href' attribute) */
+  const targetArticle = document.querySelector(articleSelector)
+
+  /* [DONE]add class 'active' to the correct article */
+  targetArticle.classList.add('active');
   
 };
 
